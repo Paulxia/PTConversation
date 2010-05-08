aa459ba73851a0d8b204dd75d1b5bc8d0b3c259c
Lasha Dolidze <lashad@mac.com>
2010-05-08 11:41:03 +0400

    Refactoring project tree

--- PTConversationViewDemo/Classes/RootViewController.h
+++ PTConversationViewDemo/Classes/RootViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "PTConversationViewController.h"
+#import "PTConversation/PTConversationViewController.h"
 
 @interface RootViewController : UITableViewController <PTConversationViewControllerDelegate> {
 	
